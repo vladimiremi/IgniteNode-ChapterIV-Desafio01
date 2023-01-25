ea39423b748f74729fc2d1663be1d2dbad9556ce
vladimiremi <vladimiremi2014@gmail.com>
2023-01-25 16:40:44 -0300

    refactor: server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
-import { app } from './app';
-
-app.listen(3333, () => { console.log('Server is running') });
+import { app } from "./app";
+const PORT = 3333;
+app.listen(PORT, () => {
+  console.log("Server is running in port " + PORT);
+});
